f45e195e2eb4a0be6651e12117af9eb070c1f222
Tim Pietrusky <timpietrusky@gmail.com>
2018-12-13 22:43:13 +0100

    test(thorium): Added example test

---
+++ tests/thorium-client.test.js
@@ -0,0 +1,8 @@
+/**
+ * @jest-environment node
+ */
+test('Test Thorium Schema', () => {
+  // Find a way to get the schema out of Thorium so that we can actually
+  // use it to test
+  expect(true).toBe(true)
+})
