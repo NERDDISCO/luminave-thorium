fc1ce14b16f9d7d77dc15d7487718bbb753b2519
Alex Anderson <alexanderson1993@gmail.com>
2019-07-22 18:57:39 -0600

    fix(Bundle): Updates the webpack config to bundle the entire program into a single JavaScript file, including node modules. This makes it easier to bundle together into a single executable or run from a script.

--- webpack.node.config.js
+++ webpack.node.config.js
@@ -1,31 +1,27 @@
 const path = require('path');
-// Don't bundle the modules from node_modules, just require them
-var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    entry: [
-      './src/index.js'
-    ],
-    target: 'node',
-    externals: [nodeExternals()],
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        publicPath: 'build/'
-    },
-    mode: "development",
-    optimization: {
-      // We no not want to minimize our code.
-      minimize: false
-    },
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                exclude: /node_modules/,
-                test: /\.js$/
-            }
-        ]
-    }
+  entry: [
+    './src/index.js'
+  ],
+  target: 'node',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: 'build/'
+  },
+  mode: "development",
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  }
 }
